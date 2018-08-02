5be11acf8e7981da2be28de26737d7a4bfc12109
gibbed <rick@gibbed.us>
2018-08-01 20:43:43 -0500

    Added initial solution & projects.

---
+++ projects/Gibbed.Yakuza0.FileFormats/ArchiveFile.cs
@@ -0,0 +1,309 @@
+﻿/* Copyright (c) 2018 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Gibbed.IO;
+
+namespace Gibbed.Yakuza0.FileFormats
+{
+    public class ArchiveFile
+    {
+        public const uint Signature = 0x50415243; // 'PARC'
+
+        private readonly List<FileEntry> _Entries;
+
+        public ArchiveFile()
+        {
+            this._Entries = new List<FileEntry>();
+        }
+
+        public List<FileEntry> Entries
+        {
+            get { return this._Entries; }
+        }
+
+        public void Serialize(Stream output)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deserialize(Stream input)
+        {
+            var basePosition = input.Position;
+
+            var magic = input.ReadValueU32(Endian.Big);
+            if (magic != Signature)
+            {
+                throw new FormatException();
+            }
+
+            var version = input.ReadValueU8();
+            if (version != 2)
+            {
+                throw new FormatException();
+            }
+
+            var endianness = input.ReadValueU8();
+            if (endianness != 0 && endianness != 1)
+            {
+                throw new FormatException();
+            }
+            var endian = endianness == 0 ? Endian.Little : Endian.Big;
+
+            var unknown06 = input.ReadValueU8();
+            var unknown07 = input.ReadValueU8();
+            if (unknown06 != 0 || unknown07 != 0)
+            {
+                throw new FormatException();
+            }
+
+            var unknown08 = input.ReadValueU16(endian);
+            var unknown0A = input.ReadValueU16(endian);
+
+            // Just guessing.
+            if (version != unknown08 || endianness != unknown0A)
+            {
+                throw new FormatException();
+            }
+
+            var unknown0C = input.ReadValueU32(endian);
+            if (unknown0C != 0)
+            {
+                throw new FormatException();
+            }
+
+            var directoryCount = input.ReadValueU32(endian);
+            var directoryTableOffset = input.ReadValueU32(endian);
+            var fileCount = input.ReadValueU32(endian);
+            var fileTableOffset = input.ReadValueU32(endian);
+
+            var directoryNames = new string[directoryCount];
+            for (uint i = 0; i < directoryCount; i++)
+            {
+                directoryNames[i] = input.ReadString(64, true, Encoding.ASCII);
+            }
+
+            var fileNames = new string[fileCount];
+            for (uint i = 0; i < fileCount; i++)
+            {
+                fileNames[i] = input.ReadString(64, true, Encoding.ASCII);
+            }
+
+            var rawDirectoryEntries = new RawDirectoryEntry[directoryCount];
+            if (directoryCount > 0)
+            {
+                input.Position = basePosition + directoryTableOffset;
+                for (uint i = 0; i < directoryCount; i++)
+                {
+                    var directoryEntry = rawDirectoryEntries[i] = RawDirectoryEntry.Read(input, endian);
+                    if (directoryEntry.Unknown14 != 0 || directoryEntry.Unknown18 != 0 || directoryEntry.Unknown1C != 0)
+                    {
+                        throw new FormatException();
+                    }
+                }
+            }
+
+            var rawFileEntries = new RawFileEntry[fileCount];
+            if (fileCount > 0)
+            {
+                input.Position = basePosition + fileTableOffset;
+                for (uint i = 0; i < fileCount; i++)
+                {
+                    var fileEntry = rawFileEntries[i] = RawFileEntry.Read(input, endian);
+                    if ((fileEntry.DataFlags & 0x7FFFFFFFu) != 0)
+                    {
+                        throw new FormatException();
+                    }
+                    if (fileEntry.Unknown14 != 0 || fileEntry.Unknown18 != 0)
+                    {
+                        throw new FormatException();
+                    }
+                }
+            }
+
+            var fileEntries = new List<FileEntry>();
+
+            if (directoryCount > 0)
+            {
+                var queue = new Queue<KeyValuePair<int, string>>();
+                queue.Enqueue(new KeyValuePair<int, string>(0, null));
+
+                while (queue.Count > 0)
+                {
+                    var directoryInfo = queue.Dequeue();
+                    var directoryIndex = directoryInfo.Key;
+                    var rawDirectoryEntry = rawDirectoryEntries[directoryIndex];
+                    var directoryName = directoryNames[directoryIndex];
+                    var isRootDirectory = directoryIndex == 0 && directoryName == ".";
+                    var directoryBasePath = directoryInfo.Value;
+                    var directoryPath = directoryBasePath == null
+                                            ? isRootDirectory == true
+                                                  ? null
+                                                  : directoryName
+                                            : isRootDirectory == true
+                                                  ? directoryBasePath
+                                                  : Path.Combine(directoryBasePath, directoryName);
+
+                    for (int i = 0, o = rawDirectoryEntry.DirectoryIndex;
+                        i < rawDirectoryEntry.DirectoryCount;
+                        i++, o++)
+                    {
+                        queue.Enqueue(new KeyValuePair<int, string>(o, directoryPath));
+                    }
+
+                    for (int i = 0, o = rawDirectoryEntry.FileIndex; i < rawDirectoryEntry.FileCount; i++, o++)
+                    {
+                        var rawFileEntry = rawFileEntries[o];
+                        FileEntry fileEntry;
+                        fileEntry.Path = directoryPath == null
+                                             ? fileNames[o]
+                                             : Path.Combine(directoryPath, fileNames[o]);
+                        fileEntry.IsCompressed = ((FileFlags)rawFileEntry.DataFlags & FileFlags.IsCompressed) != 0;
+                        fileEntry.DataUncompressedSize = rawFileEntry.DataUncompressedSize;
+                        fileEntry.DataCompressedSize = rawFileEntry.DataCompressedSize;
+                        fileEntry.DataOffset = rawFileEntry.DataOffset;
+                        fileEntry.DataHeaderSize = rawFileEntry.Unknown10;
+                        fileEntry.DataHash = rawFileEntry.Unknown1C;
+                        fileEntries.Add(fileEntry);
+                    }
+                }
+            }
+            else
+            {
+                // no directories but there's a file?
+                if (fileCount > 0)
+                {
+                    throw new FormatException();
+                }
+            }
+
+            this._Entries.Clear();
+            this._Entries.AddRange(fileEntries);
+        }
+
+        [Flags]
+        public enum FileFlags : uint
+        {
+            None = 0u,
+            IsCompressed = 1u << 31,
+        }
+
+        private struct RawDirectoryEntry
+        {
+            public int DirectoryCount;
+            public int DirectoryIndex;
+            public int FileCount;
+            public int FileIndex;
+            public uint Unknown10;
+            public uint Unknown14;
+            public uint Unknown18;
+            public uint Unknown1C;
+
+            public static RawDirectoryEntry Read(Stream input, Endian endian)
+            {
+                RawDirectoryEntry instance;
+                instance.DirectoryCount = input.ReadValueS32(endian);
+                instance.DirectoryIndex = input.ReadValueS32(endian);
+                instance.FileCount = input.ReadValueS32(endian);
+                instance.FileIndex = input.ReadValueS32(endian);
+                instance.Unknown10 = input.ReadValueU32(endian);
+                instance.Unknown14 = input.ReadValueU32(endian);
+                instance.Unknown18 = input.ReadValueU32(endian);
+                instance.Unknown1C = input.ReadValueU32(endian);
+                return instance;
+            }
+
+            public override string ToString()
+            {
+                return string.Format(
+                    "dcount={0}, dindex={1}, fcount={2}, findex={3}, {4:X}, {5:X}, {6:X}, {7:X}",
+                    this.DirectoryCount,
+                    this.DirectoryIndex,
+                    this.FileCount,
+                    this.FileIndex,
+                    this.Unknown10,
+                    this.Unknown14,
+                    this.Unknown18,
+                    this.Unknown1C);
+            }
+        }
+
+        private struct RawFileEntry
+        {
+            public uint DataFlags;
+            public uint DataUncompressedSize;
+            public uint DataCompressedSize;
+            public uint DataOffset;
+            public uint Unknown10; // probably header size?
+            public uint Unknown14;
+            public uint Unknown18;
+            public uint Unknown1C; // probably data hash?
+
+            public static RawFileEntry Read(Stream input, Endian endian)
+            {
+                RawFileEntry instance;
+                instance.DataFlags = input.ReadValueU32(endian);
+                instance.DataUncompressedSize = input.ReadValueU32(endian);
+                instance.DataCompressedSize = input.ReadValueU32(endian);
+                instance.DataOffset = input.ReadValueU32(endian);
+                instance.Unknown10 = input.ReadValueU32(endian);
+                instance.Unknown14 = input.ReadValueU32(endian);
+                instance.Unknown18 = input.ReadValueU32(endian);
+                instance.Unknown1C = input.ReadValueU32(endian);
+                return instance;
+            }
+
+            public override string ToString()
+            {
+                return string.Format(
+                    "flags={0:X}, usize={1:X}, csize={2:X}, offset={3:X}, {4:X}, {5:X}, {6:X}, {7:X}",
+                    this.DataFlags,
+                    this.DataUncompressedSize,
+                    this.DataCompressedSize,
+                    this.DataOffset,
+                    this.Unknown10,
+                    this.Unknown14,
+                    this.Unknown18,
+                    this.Unknown1C);
+            }
+        }
+
+        public struct FileEntry
+        {
+            public string Path;
+            public bool IsCompressed;
+            public uint DataUncompressedSize;
+            public uint DataCompressedSize;
+            public uint DataOffset;
+            public uint DataHeaderSize;
+            public uint DataHash;
+
+            public override string ToString()
+            {
+                return this.Path ?? base.ToString();
+            }
+        }
+    }
+}
---
+++ projects/Gibbed.Yakuza0.Unpack/Program.cs
@@ -0,0 +1,241 @@
+﻿/* Copyright (c) 2018 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using Gibbed.IO;
+using Gibbed.Yakuza0.FileFormats;
+using NDesk.Options;
+
+namespace Gibbed.Yakuza0.Unpack
+{
+    internal class Program
+    {
+        private static string GetExecutableName()
+        {
+            return Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        }
+
+        public static void Main(string[] args)
+        {
+            bool showHelp = false;
+            bool overwriteFiles = false;
+            bool verbose = false;
+
+            var options = new OptionSet()
+            {
+                { "o|overwrite", "overwrite existing files", v => overwriteFiles = v != null },
+                { "v|verbose", "be verbose", v => verbose = v != null },
+                { "h|help", "show this message and exit", v => showHelp = v != null },
+            };
+
+            List<string> extras;
+
+            try
+            {
+                extras = options.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("{0}: ", GetExecutableName());
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try `{0} --help' for more information.", GetExecutableName());
+                return;
+            }
+
+            if (extras.Count < 1 || extras.Count > 2 || showHelp == true)
+            {
+                Console.WriteLine("Usage: {0} [OPTIONS]+ input_par [output_dir]", GetExecutableName());
+                Console.WriteLine();
+                Console.WriteLine("Options:");
+                options.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+            var inputPath = Path.GetFullPath(extras[0]);
+            var outputPath = extras.Count > 1 ? extras[1] : Path.ChangeExtension(inputPath, null) + "_unpack";
+
+            using (var input = File.OpenRead(inputPath))
+            {
+                var archive = new ArchiveFile();
+                archive.Deserialize(input);
+
+                long current = 0;
+                long total = archive.Entries.Count;
+                var padding = total.ToString(CultureInfo.InvariantCulture).Length;
+
+                foreach (var entry in archive.Entries)
+                {
+                    current++;
+
+                    var entryPath = Path.Combine(outputPath, entry.Path);
+                    if (overwriteFiles == false && File.Exists(entryPath) == true)
+                    {
+                        continue;
+                    }
+
+                    if (verbose == true)
+                    {
+                        Console.WriteLine(
+                            "[{0}/{1}] {2}",
+                            current.ToString(CultureInfo.InvariantCulture).PadLeft(padding),
+                            total,
+                            entry.Path);
+                    }
+
+                    var entryDirectory = Path.GetDirectoryName(entryPath);
+                    if (entryDirectory != null)
+                    {
+                        Directory.CreateDirectory(entryDirectory);
+                    }
+
+                    using (var output = File.Create(entryPath))
+                    {
+                        input.Seek(entry.DataOffset, SeekOrigin.Begin);
+
+                        if (entry.IsCompressed == false)
+                        {
+                            output.WriteFromStream(input, entry.DataCompressedSize);
+                        }
+                        else
+                        {
+                            Decompress(input, entry, output);
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void Decompress(Stream input, ArchiveFile.FileEntry entry, Stream output)
+        {
+            const uint signature = 0x534C4C5A; // 'SLLZ'
+
+            var magic = input.ReadValueU32(Endian.Big);
+            if (magic != signature)
+            {
+                throw new FormatException();
+            }
+
+            var endianness = input.ReadValueU8();
+            if (endianness != 0 && endianness != 1)
+            {
+                throw new FormatException();
+            }
+            var endian = endianness == 0 ? Endian.Little : Endian.Big;
+
+            var version = input.ReadValueU8();
+            if (version != 1)
+            {
+                throw new FormatException();
+            }
+
+            var headerSize = input.ReadValueU16(endian);
+            if (headerSize != 16)
+            {
+                throw new FormatException();
+            }
+
+            var uncompressedSize = input.ReadValueU32(endian);
+            var compressedSize = input.ReadValueU32(endian);
+
+            if (entry.DataUncompressedSize != uncompressedSize || entry.DataCompressedSize != compressedSize)
+            {
+                throw new FormatException();
+            }
+
+            compressedSize -= 16; // compressed size includes SLLZ header
+
+            var block = new byte[18];
+            long compressedCount = 0;
+            long uncompressedCount = 0;
+
+            byte opFlags = input.ReadValueU8();
+            compressedCount++;
+            int opBits = 8;
+
+            int literalCount = 0;
+            while (compressedCount < compressedSize)
+            {
+                var isCopy = (opFlags & 0x80) != 0;
+                opFlags <<= 1;
+                opBits--;
+
+                if (opBits == 0)
+                {
+                    if (literalCount > 0)
+                    {
+                        input.Read(block, 0, literalCount);
+                        output.Write(block, 0, literalCount);
+                        uncompressedCount += literalCount;
+                        literalCount = 0;
+                    }
+
+                    opFlags = input.ReadValueU8();
+                    compressedCount++;
+                    opBits = 8;
+                }
+
+                if (isCopy == false)
+                {
+                    literalCount++;
+                    compressedCount++;
+                    continue;
+                }
+
+                if (literalCount > 0)
+                {
+                    input.Read(block, 0, literalCount);
+                    output.Write(block, 0, literalCount);
+                    uncompressedCount += literalCount;
+                    literalCount = 0;
+                }
+
+                var copyFlags = input.ReadValueU16(Endian.Little);
+                compressedCount += 2;
+
+                var copyDistance = 1 + (copyFlags >> 4);
+                var copyCount = 3 + (copyFlags & 0xF);
+
+                var originalPosition = output.Position;
+                output.Position = output.Length - copyDistance;
+                output.Read(block, 0, copyCount);
+                output.Position = originalPosition;
+                output.Write(block, 0, copyCount);
+                uncompressedCount += copyCount;
+            }
+
+            if (literalCount > 0)
+            {
+                input.Read(block, 0, literalCount);
+                output.Write(block, 0, literalCount);
+                uncompressedCount += literalCount;
+            }
+
+            if (uncompressedCount != uncompressedSize)
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+}
