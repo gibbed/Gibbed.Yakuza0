1613696b980ae4e3784754a010315f106aa21ad0
gibbed <rick@gibbed.us>
2018-08-03 17:01:24 -0500

    Implemented serialization for ArchiveFile.

--- projects/Gibbed.Yakuza0.FileFormats/ArchiveFile.cs
+++ projects/Gibbed.Yakuza0.FileFormats/ArchiveFile.cs
@@ -23,6 +23,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using Gibbed.IO;
 
@@ -32,6 +33,7 @@ namespace Gibbed.Yakuza0.FileFormats
     {
         public const uint Signature = 0x50415243; // 'PARC'
 
+        private Endian _Endian = Endian.Little;
         private readonly List<FileEntry> _Entries;
 
         public ArchiveFile()
@@ -39,14 +41,263 @@ namespace Gibbed.Yakuza0.FileFormats
             this._Entries = new List<FileEntry>();
         }
 
+        public Endian Endian
+        {
+            get { return this._Endian; }
+            set { this._Endian = value; }
+        }
+
         public List<FileEntry> Entries
         {
             get { return this._Entries; }
         }
 
+        private struct DirectoryEstimate
+        {
+            public int DirectoryCount;
+            public int FileCount;
+        }
+
+        public static long EstimateHeaderSize(IEnumerable<string> paths)
+        {
+            var counts = new Dictionary<string, DirectoryEstimate>();
+
+            DirectoryEstimate rootCount;
+            rootCount.DirectoryCount = 1; // include itself
+            rootCount.FileCount = 0;
+
+            foreach (var path in paths)
+            {
+                var parts = path.Split('\\');
+
+                var count = parts.Length;
+                if (count <= 1)
+                {
+                    rootCount.FileCount++;
+                    continue;
+                }
+
+                DirectoryEstimate directoryEstimate;
+
+                string directoryPath = "";
+                for (int i = 0; i < count - 2; i++)
+                {
+                    directoryPath += parts[i].ToLowerInvariant() + '\\';
+                    counts.TryGetValue(directoryPath, out directoryEstimate);
+                    directoryEstimate.DirectoryCount++;
+                    counts[directoryPath] = directoryEstimate;
+                }
+
+                directoryPath += parts[count - 2].ToLowerInvariant() + '\\';
+                if (counts.TryGetValue(directoryPath, out directoryEstimate) == false)
+                {
+                    directoryEstimate.DirectoryCount = 1; // include itself
+                }
+                directoryEstimate.FileCount++;
+                counts[directoryPath] = directoryEstimate;
+            }
+
+            var totalPathCount = rootCount.DirectoryCount + counts.Sum(kv => kv.Value.DirectoryCount);
+            var totalFileCount = rootCount.FileCount + counts.Sum(kv => kv.Value.FileCount);
+
+            return (32) + // header
+                   (64 * totalPathCount) + // directory names
+                   (64 * totalFileCount) + // file names
+                   (32 * totalPathCount) + // directory entries
+                   (32 * totalFileCount); // file entries
+        }
+
+        private class NewDirectoryEntry
+        {
+            private string _Name;
+            private readonly Dictionary<string, NewDirectoryEntry> _Subdirectories;
+            private readonly Dictionary<string, KeyValuePair<string, FileEntry>> _Files;
+
+            public NewDirectoryEntry()
+            {
+                this._Subdirectories = new Dictionary<string, NewDirectoryEntry>();
+                this._Files = new Dictionary<string, KeyValuePair<string, FileEntry>>();
+            }
+
+            public string Name
+            {
+                get { return this._Name; }
+                set { this._Name = value; }
+            }
+
+            public Dictionary<string, NewDirectoryEntry> Subdirectories
+            {
+                get { return this._Subdirectories; }
+            }
+
+            public Dictionary<string, KeyValuePair<string, FileEntry>> Files
+            {
+                get { return this._Files; }
+            }
+        }
+
         public void Serialize(Stream output)
         {
-            throw new NotImplementedException();
+            var rootDirectoryEntry = new NewDirectoryEntry();
+
+            foreach (var fileEntry in this._Entries)
+            {
+                var directoryEntry = rootDirectoryEntry;
+
+                var parts = fileEntry.Path.Split('\\');
+                var partCount = parts.Length;
+                if (partCount <= 0)
+                {
+                    continue;
+                }
+
+                if (partCount >= 2)
+                {
+                    for (int i = 0; i < partCount - 1; i++)
+                    {
+                        var subdirectoryKey = parts[i].ToLowerInvariant();
+
+                        NewDirectoryEntry subdirectory;
+                        if (directoryEntry.Subdirectories.TryGetValue(subdirectoryKey, out subdirectory) == false)
+                        {
+                            subdirectory = directoryEntry.Subdirectories[subdirectoryKey] = new NewDirectoryEntry()
+                            {
+                                Name = parts[i],
+                            };
+                        }
+                        directoryEntry = subdirectory;
+                    }
+                }
+
+                var fileName = parts[partCount - 1];
+                var fileKey = fileName.ToLowerInvariant();
+                directoryEntry.Files.Add(fileKey, new KeyValuePair<string, FileEntry>(fileName, fileEntry));
+            }
+
+            var directoryNames = new List<string>();
+            var fileNames = new List<string>();
+            var rawDirectoryEntries = new List<RawDirectoryEntry>();
+            var rawFileEntries = new List<RawFileEntry>();
+
+            var queue = new Queue<KeyValuePair<string, NewDirectoryEntry>>();
+            queue.Enqueue(new KeyValuePair<string, NewDirectoryEntry>(".", rootDirectoryEntry));
+
+            while (queue.Count > 0)
+            {
+                var pair = queue.Dequeue();
+                var directoryName = pair.Key;
+                var directoryEntry = pair.Value;
+
+                RawDirectoryEntry rawDirectoryEntry;
+                rawDirectoryEntry.DirectoryIndex = rawDirectoryEntries.Count + 1;
+                rawDirectoryEntry.DirectoryCount = directoryEntry.Subdirectories.Count;
+                rawDirectoryEntry.FileIndex = rawFileEntries.Count;
+                rawDirectoryEntry.FileCount = directoryEntry.Files.Count;
+                rawDirectoryEntry.Unknown10 = 0;
+                rawDirectoryEntry.Unknown14 = 0;
+                rawDirectoryEntry.Unknown18 = 0;
+                rawDirectoryEntry.Unknown1C = 0;
+
+                rawDirectoryEntries.Add(rawDirectoryEntry);
+                directoryNames.Add(directoryName);
+
+                foreach (var kv in directoryEntry.Subdirectories)
+                {
+                    queue.Enqueue(kv);
+                }
+
+                foreach (var kv in directoryEntry.Files.Values)
+                {
+                    var fileName = kv.Key;
+                    var fileEntry = kv.Value;
+
+                    RawFileEntry rawFileEntry;
+                    rawFileEntry.DataFlags = fileEntry.IsCompressed == true ? 0x80000000u : 0u;
+                    rawFileEntry.DataUncompressedSize = fileEntry.DataUncompressedSize;
+                    rawFileEntry.DataCompressedSize = fileEntry.DataCompressedSize;
+                    rawFileEntry.DataOffset = fileEntry.DataOffset;
+                    rawFileEntry.Unknown10 = fileEntry.DataHeaderSize;
+                    rawFileEntry.Unknown14 = 0;
+                    rawFileEntry.Unknown18 = 0;
+                    rawFileEntry.Unknown1C = fileEntry.DataHash;
+
+                    rawFileEntries.Add(rawFileEntry);
+                    fileNames.Add(fileName);
+                }
+            }
+
+            var endian = this._Endian;
+
+            byte[] directoryTableBytes;
+            using (var temp = new MemoryStream())
+            {
+                foreach (var rawDirectoryEntry in rawDirectoryEntries)
+                {
+                    rawDirectoryEntry.Write(temp, endian);
+                }
+                temp.Flush();
+                directoryTableBytes = temp.ToArray();
+            }
+
+            byte[] fileTableBytes;
+            using (var temp = new MemoryStream())
+            {
+                foreach (var rawFileEntry in rawFileEntries)
+                {
+                    rawFileEntry.Write(temp, endian);
+                }
+                temp.Flush();
+                fileTableBytes = temp.ToArray();
+            }
+
+            var endianness = GetEndianness(endian);
+
+            var headerSize = 32;
+            headerSize += 64 * rawDirectoryEntries.Count;
+            headerSize += 64 * rawFileEntries.Count;
+
+            output.WriteValueU32(Signature, Endian.Big);
+            output.WriteValueU8(2);
+            output.WriteValueU8(endianness);
+            output.WriteValueU8(0);
+            output.WriteValueU8(0);
+            output.WriteValueU32(0x00020001u, endian);
+            output.WriteValueU32(0, endian);
+            output.WriteValueS32(rawDirectoryEntries.Count, endian);
+            output.WriteValueS32(headerSize, endian);
+            output.WriteValueS32(rawFileEntries.Count, endian);
+            output.WriteValueS32(headerSize + directoryTableBytes.Length, endian);
+
+            foreach (var directoryName in directoryNames)
+            {
+                output.WriteString(directoryName, 64, Encoding.ASCII);
+            }
+
+            foreach (var fileName in fileNames)
+            {
+                output.WriteString(fileName, 64, Encoding.ASCII);
+            }
+
+            output.WriteBytes(directoryTableBytes);
+            output.WriteBytes(fileTableBytes);
+        }
+
+        private static byte GetEndianness(Endian endian)
+        {
+            switch (endian)
+            {
+                case Endian.Little:
+                {
+                    return 0;
+                }
+
+                case Endian.Big:
+                {
+                    return 1;
+                }
+            }
+
+            throw new NotSupportedException();
         }
 
         public void Deserialize(Stream input)
@@ -79,11 +330,8 @@ namespace Gibbed.Yakuza0.FileFormats
                 throw new FormatException();
             }
 
-            var unknown08 = input.ReadValueU16(endian);
-            var unknown0A = input.ReadValueU16(endian);
-
-            // Just guessing.
-            if (version != unknown08 || endianness != unknown0A)
+            var unknown08 = input.ReadValueU32(endian);
+            if (unknown08 != 0x00020001u)
             {
                 throw new FormatException();
             }
@@ -199,6 +447,7 @@ namespace Gibbed.Yakuza0.FileFormats
                 }
             }
 
+            this._Endian = endian;
             this._Entries.Clear();
             this._Entries.AddRange(fileEntries);
         }
@@ -233,6 +482,23 @@ namespace Gibbed.Yakuza0.FileFormats
                 instance.Unknown18 = input.ReadValueU32(endian);
                 instance.Unknown1C = input.ReadValueU32(endian);
                 return instance;
+            }
+
+            public static void Write(Stream output, RawDirectoryEntry instance, Endian endian)
+            {
+                output.WriteValueS32(instance.DirectoryCount, endian);
+                output.WriteValueS32(instance.DirectoryIndex, endian);
+                output.WriteValueS32(instance.FileCount, endian);
+                output.WriteValueS32(instance.FileIndex, endian);
+                output.WriteValueU32(instance.Unknown10, endian);
+                output.WriteValueU32(instance.Unknown14, endian);
+                output.WriteValueU32(instance.Unknown18, endian);
+                output.WriteValueU32(instance.Unknown1C, endian);
+            }
+
+            public void Write(Stream output, Endian endian)
+            {
+                Write(output, this, endian);
             }
 
             public override string ToString()
@@ -273,6 +539,23 @@ namespace Gibbed.Yakuza0.FileFormats
                 instance.Unknown18 = input.ReadValueU32(endian);
                 instance.Unknown1C = input.ReadValueU32(endian);
                 return instance;
+            }
+
+            public static void Write(Stream output, RawFileEntry instance, Endian endian)
+            {
+                output.WriteValueU32(instance.DataFlags, endian);
+                output.WriteValueU32(instance.DataUncompressedSize, endian);
+                output.WriteValueU32(instance.DataCompressedSize, endian);
+                output.WriteValueU32(instance.DataOffset, endian);
+                output.WriteValueU32(instance.Unknown10, endian);
+                output.WriteValueU32(instance.Unknown14, endian);
+                output.WriteValueU32(instance.Unknown18, endian);
+                output.WriteValueU32(instance.Unknown1C, endian);
+            }
+
+            public void Write(Stream output, Endian endian)
+            {
+                Write(output, this, endian);
             }
 
             public override string ToString()
